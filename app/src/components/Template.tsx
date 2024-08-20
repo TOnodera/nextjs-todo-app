6661a27df7e9af8c994737d1c18b9cace9e05a01
onodera <onodera@h-company.co.jp>
2024-08-20 10:27:03 +0000

    theme設定

--- app/src/components/Template.tsx
+++ app/src/components/Template.tsx
@@ -1,13 +1,12 @@
 "use client";
-import { Person } from "@mui/icons-material";
 import {
   CssBaseline,
   CssVarsProvider,
   Divider,
   extendTheme,
   Grid,
-  Typography,
 } from "@mui/joy";
+import AddTaskSharpIcon from "@mui/icons-material/AddTaskSharp";
 
 interface Props {
   children: React.ReactNode;
@@ -15,6 +14,22 @@ interface Props {
 export default function Template({ children }: Props) {
   const theme = extendTheme({
     colorSchemes: {
+      light: {
+        palette: {
+          primary: {
+            50: "#C0CCD9",
+            100: "#A5B8CF",
+            200: "#6A96CA",
+            300: "#4886D0",
+            400: "#2178DD",
+            500: "#096BDE",
+            600: "#1B62B5",
+            700: "#265995",
+            800: "#2F4968",
+            900: "#2F3C4C",
+          },
+        },
+      },
       dark: {
         palette: {
           primary: {
@@ -39,13 +54,20 @@ export default function Template({ children }: Props) {
         <Grid container direction="column" sx={{ height: "100%" }}>
           <Grid>
             <>
-              <Grid container>
+              <Grid
+                container
+                alignItems="center"
+                sx={(theme) => ({
+                  padding: "1rem",
+                  backgroundColor:
+                    theme.colorSchemes.light.palette.primary[100],
+                  color: "white",
+                })}
+              >
                 <Grid>
-                  <Person />
+                  <AddTaskSharpIcon />
                 </Grid>
-                <Grid>
-                  <Typography variant="outlined">Todo App</Typography>
-                </Grid>
+                <Grid>Todo App</Grid>
               </Grid>
             </>
           </Grid>
